96cf6236a401ca7dc590aede75f04d0ca7c25a9d
legitblonde <63757114+legitblonde@users.noreply.github.com>
2020-05-06 13:38:49 -0600

    Fix layout and logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+  expression = '';
+
+  btnPressed1(inValue) {
+    this.expression = this.expression + inValue;
+  }
+
+  btnPressed2(inValue) {
+    this.expression = this.expression + ' ' + inValue + ' ';
+    console.log('--' + this.expression + '--');
+  }
+
+  btnDeletePressed() {
+    let theLength = this.expression.length;
+    if (this.expression.endsWith(' ')) {
+      theLength = theLength - 3;
+    } else {
+      theLength = theLength - 1;
+    }
+    this.expression = this.expression.substring(0, theLength);
+  }
+
+  evaluate() {
+    const theData = this.expression.split(' ');
+
+    let theOperator: string;
+    let theLeft: number;
+    let theTotal = 0.0;
+    for (const theValue of theData) {
+      if (!isNaN(+theValue)) {
+        if (theLeft === undefined){
+          theLeft = +theValue;
+        } else {
+          switch (theOperator) {
+            case '/':
+              theTotal = theLeft / +theValue;
+              break;
+            case '*':
+              theTotal = theLeft * +theValue;
+              break;
+            case '-':
+              theTotal = theLeft - +theValue;
+              break;
+            case '+':
+              theTotal = theLeft + +theValue;
+              break;
+          }
+          theLeft = theTotal;
+        }
+      } else {
+        theOperator = theValue;
+      }
+
+    }
+    this.expression = theTotal.toString();
+  }
 }
